86bfeaa29455287997a7c7973fb9a1353d490908
Antonella Garay <65576733+AntonellaAB@users.noreply.github.com>
2026-05-23 12:55:19 -0300

    Change database password
    Updated database password for connection.

--- src/db/conexionDB.java
+++ src/db/conexionDB.java
@@ -11,7 +11,7 @@ public class conexionDB {
     private static final String URL = "jdbc:postgresql://localhost:5432/product_review_db";
     //URL - JDBC java Database Connectivity : hay que descargar el driver 
     private static final String USER = "review_app_user";
-    private static final String PASSWORD = "h0l4@574rW4r5";
+    private static final String PASSWORD = "password";
 
     public static Connection conectar() {
         Connection conn = null; //significa que inicialmente no hay conexion
