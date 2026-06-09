7e71862e804beee029a558e03b7e08b99dd3d1af
FaMaah <fabriziomazo05@gmail.com>
2026-06-08 22:12:17 -0300

    Review DAO terminado Fabri

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,87 +1,46 @@
-
 package main;
 //import db.conexionDB;
 
 import dao.UserDAO;
-<<<<<<< HEAD
 import model.User;
-import dao.ReviewDAO;
-import model.Review;
-=======
 import ui.Login;
 //import model.User;
 
->>>>>>> origin/main
 
 public class Main {
-    
+
     public static void main(String[] args) {
-<<<<<<< HEAD
-        /*/crear un nuevo usuario
-=======
         java.awt.EventQueue.invokeLater(() -> {
             // 1. Instanciamos la ventana del Login
             Login ventanaLogin = new Login();
-            
+
             // 2. La hacemos visible en la pantalla
             ventanaLogin.setVisible(true);
         });
         
         /*
-        crear un nuevo usuario
->>>>>>> origin/main
-        
+        //crear un nuevo usuario
+
         User user = new User();
-        user.setUsername("Fabrizio");
-        user.setEmail("fabriziomazo05@gmail.com");
-        user.setPassword("a");
+
+        user.setUsername("Fabri");
+        user.setEmail("pepeantonio45@gmail.com");
+        user.setPassword("1234");
 
         UserDAO dao = new UserDAO();
 
         dao.createUser(user); //Le pasa el objeto User al DAO.
-<<<<<<< HEAD
-=======
+
+
         
->>>>>>> origin/main
-        
-        
-        
-        // Probar el login 
+        //Probar el login 
         UserDAO dao = new UserDAO();
-        boolean access = dao.login("Fabrizio", "a");
+        boolean access = dao.login("Nayeli", "1234");
+
         System.out.println(access);
         */
-<<<<<<< HEAD
-        System.out.println("=== INICIANDO PRUEBA DEL ALGORITMO DE REVIEWS ===");
-        
-        // 1. Creamos el objeto con datos de prueba
-        Review pruebaReview = new Review();
-        pruebaReview.setComments("¡Excelente base de datos! Funciona muy rápido de forma local.");
-        pruebaReview.setScore(5);
 
-        // SOLUCIÓN: Creamos un usuario ficticio con el ID que queremos guardar
-        User usuarioSimulado = new User();
-        usuarioSimulado.setIdUser(2); // El ID de usuario que ya existe en tu BD de pgAdmin
 
-        // Le pasamos el usuario a la review
-        pruebaReview.setUser(usuarioSimulado); 
 
-        // 2. Instanciamos tu DAO
-        ReviewDAO dao = new ReviewDAO();
-
-        // 3. Ejecutamos tu algoritmo de guardado
-        boolean resultado = dao.guardarReview(pruebaReview);
-        
-        // 4. Verificamos la respuesta en la consola de NetBeans
-        if (resultado) {
-            System.out.println("=== PRUEBA FINALIZADA: Revisa tu pgAdmin para confirmar ===");
-        } else {
-            System.out.println("=== PRUEBA FALLIDA: Revisa las letras rojas del error ===");
-        }
-=======
-        
-        
-        
->>>>>>> origin/main
     }
 }
